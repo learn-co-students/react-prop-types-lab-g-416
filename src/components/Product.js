39272801e84aefd319e6bcb756ffe04e21162218
fut0k0 <cccabell@icloud.com>
2020-04-27 09:51:30 -0700

    Passing lab

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,1 +1,37 @@
-// Code Product Component Here
+import React from "react";
+import PropTypes from "prop-types";
+
+class Product extends React.Component {
+	render() {
+		return (
+			<div>
+			</div>
+		)
+	}
+}
+
+const colors = ["white", "eggshell-white", "salmon"]
+
+function weightValidation(props, propName, componentName) {
+	const value = props[propName]
+	
+	if ((typeof value === "number") && (value >= 80) && (value <= 300)) {
+		return null
+	} else {
+		return "error"
+	}
+}
+
+Product.defaultProps = {
+	hasWatermark: false
+}
+
+Product.propTypes = {
+	name: PropTypes.string.isRequired,
+	producer: PropTypes.string,
+	hasWatermark: PropTypes.bool,
+	color: PropTypes.oneOf(colors).isRequired,
+	weight: weightValidation
+}
+
+export default Product
